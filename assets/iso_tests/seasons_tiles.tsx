54d7793e06a2f942eae114c6d4ec7e337271503c
Weird Constructor <weirdconstructor@gmail.com>
2019-10-19 21:53:10 +0200

    played around with blender isometric rendering

---
+++ assets/iso_tests/seasons_tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.5" name="seasons_tiles" tilewidth="128" tileheight="64" tilecount="96" columns="8">
+ <image source="../../../../Downloads/seasons_tiles.png" width="1024" height="768"/>
+</tileset>
---
+++ assets/iso_tests/test.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.5" name="test" tilewidth="128" tileheight="128" tilecount="1" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="1">
+  <image width="128" height="128" source="../../../../../../tmp/0001.png"/>
+ </tile>
+</tileset>
