562bb32a99cf7025c8635a3e9d8c57d8a7f5bb9e
Weird Constructor <weirdconstructor@gmail.com>
2019-10-19 20:39:18 +0200

    working on save/load

---
+++ src/util.rs
@@ -0,0 +1,38 @@
+
+pub fn write_file_safely(filename: &str, s: &str) -> std::io::Result<()> {
+    use std::io::Write;
+    let tmpfile = format!("{}~", filename);
+    let mut file = std::fs::File::create(tmpfile.clone())?;
+    file.write_all(s.as_bytes())?;
+    std::fs::rename(tmpfile, filename)?;
+    Ok(())
+}
+
+pub fn read_vval_json_file(filename: &str) -> VVal {
+    match std::fs::File::open("tracker.json") {
+        Ok(mut file) => {
+            let mut c = String::new();
+            match file.read_to_string(&mut c) {
+                Ok(_) => {
+                    match VVal::from_json(&c) {
+                        Ok(v) => {
+                            v
+                        },
+                        Err(e) => {
+                            println!("SAVE DESERIALIZE ERROR: {}", e);
+                            VVal::Nul
+                        },
+                    }
+                },
+                Err(e) => {
+                    println!("SAVE READ ERROR: {}", e);
+                    VVal::Nul
+                }
+            }
+        },
+        Err(e) => {
+            println!("SAVE OPEN ERROR: {}", e);
+            VVal::Nul
+        }
+    }
+}
