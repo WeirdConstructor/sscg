2970d0c9ce058ce895bda955224e9f3e8a5a479a
Weird Constructor <weirdconstructor@gmail.com>
2019-10-22 19:48:21 +0200

    noted down a simple GUI concept

--- src/logic.rs
+++ src/logic.rs
@@ -293,12 +293,25 @@ pub trait GamePainter {
     fn push_add_offs(&mut self, xo: i32, yo: i32);
     fn pop_offs(&mut self);
     fn get_screen_pos(&self, xo: i32, yo: i32) -> (i32, i32);
-    fn draw_rect(&mut self, xo: i32, yo: i32, w: u32, h: u32, color: (u8, u8, u8, u8));
-    fn draw_rect_filled(&mut self, xo: i32, yo: i32, w: u32, h: u32, color: (u8, u8, u8, u8));
+    fn get_sprite_size(&self, id: usize) -> (u32, u32);
+    fn draw_sprite(&mut self, xo: i32, yo: i32, w: u32, h: u32, id: usize) {
+        self.draw_sprite_ex(xo, yo, w, h, id,  0.0, false, false);
+    }
+    fn draw_sprite_ex(&mut self, xo: i32, yo: i32, w: u32, h: u32,
+                      id: usize, angle: f64, flip_h: bool, flip_v: bool);
+    fn draw_rect(&mut self, xo: i32, yo: i32, w: u32, h: u32,
+                 color: (u8, u8, u8, u8));
+    fn draw_rect_filled(&mut self, xo: i32, yo: i32, w: u32, h: u32,
+                        color: (u8, u8, u8, u8));
     fn draw_dot(&mut self, xo: i32, yo: i32, r: u32, color: (u8, u8, u8, u8));
     fn draw_circle(&mut self, xo: i32, yo: i32, r: u32, color: (u8, u8, u8, u8));
-    fn draw_line(&mut self, xo: i32, yo: i32, x2o: i32, y2o: i32, t: u32, color: (u8, u8, u8, u8));
-    fn draw_text(&mut self, xo: i32, yo: i32, max_w: u32, fg: (u8, u8, u8, u8), bg: Option<(u8, u8, u8, u8)>, txt: &str);
+    fn draw_line(&mut self, xo: i32, yo: i32, x2o: i32, y2o: i32, t: u32,
+                 color: (u8, u8, u8, u8));
+    fn text_size(&mut self, txt: &str) -> (u32, u32);
+    fn draw_text(&mut self, xo: i32, yo: i32, max_w: u32,
+                 fg: (u8, u8, u8, u8),
+                 bg: Option<(u8, u8, u8, u8)>,
+                 txt: &str);
 }
 
 #[derive(Debug, PartialEq, Copy, Clone)]
