45460a90166ed6912512a0ec57271f90c875a0f9
Weird Constructor <weirdconstructor@gmail.com>
2019-11-11 20:54:13 +0100

    just some work on the inputs

---
+++ godot_project/scenes/entities/SelectorScript.gd
@@ -0,0 +1,28 @@
+extends Area
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+func _area_entered(area):
+	print("AREA ENTE");
+
+func _mouse_entered():
+	print("MOUSE ENTER")
+	self.get_parent().get_child(1).show()
+	
+func _mouse_exited():
+	print("MOUSE ENTER")
+	self.get_parent().get_child(1).hide()
+	
+func _input_event(camera, event, click_pos, cl, pre):
+	print("EVE", event);
+	self.get_parent().get_child(1).show()
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	self.get_parent().get_child(1).hide()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
