f989503a859eb2f77d9af3e6a831c2bb5992b27b
Weird Constructor <weirdconstructor@gmail.com>
2019-11-11 20:54:13 +0100

    just some work on the inputs

---
+++ godot_project/scenes/ships/ship_1.gd
@@ -0,0 +1,22 @@
+extends Spatial
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+var speed = Vector2(0, 0);
+
+func _physics_process(delta):
+	if Input.is_action_pressed("move_forward"):
+		#speed.
+	
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
