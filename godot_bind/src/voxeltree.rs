04b023709c22703b7b6e65e68cc221043c1bcdb0
Weird Constructor <weirdconstructor@gmail.com>
2019-12-14 20:53:20 +0100

    made sure n+1 step works, hoping the rest works too :-)

--- godot_bind/src/voxeltree.rs
+++ godot_bind/src/voxeltree.rs
@@ -107,11 +107,32 @@ impl<C> Vol<C> where C: VoxelColor {
     }
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Default)]
+#[derive(Clone, Copy, PartialEq, Default)]
 struct Pos {
     x: usize,
     y: usize,
     z: usize,
+}
+
+impl std::fmt::Debug for Pos {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        use std::fmt;
+        write!(f, "({}, {}, {})", self.x, self.y, self.z)
+    }
+}
+
+impl Pos {
+    pub fn new(x: usize, y: usize, z: usize) -> Self {
+        Self { x, y, z }
+    }
+
+    pub fn offs(&self, xo: usize, yo: usize, zo: usize) -> Self {
+        Self { x: self.x + xo, y: self.y + yo, z: self.z + zo }
+    }
+
+    pub fn mul(&self, m: usize) -> Self {
+        Self { x: self.x * m, y: self.y * m, z: self.z * m }
+    }
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Default)]
@@ -156,31 +177,34 @@ impl<C> Tree<C> where C: VoxelColor {
         }
     }
 
-    pub fn draw<F>(&self, f: &mut F) where F: FnMut(usize, usize, usize, usize, Voxel<C>) -> () {
-        self.draw_level(1, Pos { x: 0, y: 0, z: 0 }, f);
+    pub fn draw<F>(&self, f: &mut F) where F: FnMut(usize, &Pos, Voxel<C>) -> () {
+        self.draw_level(
+            1,
+            self.vol.size,
+            Pos { x: 0, y: 0, z: 0 },
+            Pos { x: 0, y: 0, z: 0 },
+            f);
     }
 
-    pub fn draw_level<F>(&self, level: usize, top_left: Pos, f: &mut F)
-        where F: FnMut(usize, usize, usize, usize, Voxel<C>) -> ()
+    pub fn draw_level<F>(&self, level: usize, size: usize, tree_pos: Pos, top_left: Pos, f: &mut F)
+        where F: FnMut(usize, &Pos, Voxel<C>) -> ()
     {
+        println!("DRAW lvl={}, size={}, tp={:?}, tl={:?}",
+                 level, size, tree_pos, top_left);
         if level == self.vol.size {
-            for z in 0..2 {
-                for y in 0..2 {
-                    for x in 0..2 {
-                        f(level, top_left.x + x, top_left.y + y, top_left.z + z,
-                          *self.vol.at(Pos {
-                               x: top_left.x + x,
-                               y: top_left.y + y,
-                               z: top_left.z + z
-                           }));
-                    }
-                }
-            }
+//            for z in 0..2 {
+//                for y in 0..2 {
+//                    for x in 0..2 {
+//                        let npos = top_left.offs(x, y, z);
+                        f(1, &top_left, *self.vol.at(top_left));
+//                    }
+//                }
+//            }
         } else {
-            let n = *self.node_at(level, top_left);
+            let n = *self.node_at(level, tree_pos);
             if n.empty { return; }
             if let Some(v) = n.voxel {
-                f(level, top_left.x, top_left.y, top_left.z, v);
+                f(size, &top_left, v);
 
             } else {
                 for z in 0..2 {
@@ -188,11 +212,12 @@ impl<C> Tree<C> where C: VoxelColor {
                         for x in 0..2 {
                             self.draw_level(
                                 level << 1,
-                                Pos {
-                                    x: top_left.x + x,
-                                    y: top_left.y * level + y,
-                                    z: top_left.z * level * level + z,
-                                },
+                                size >> 1,
+                                tree_pos.offs(x, y, z),
+                                top_left.offs(
+                                    x * level,
+                                    y * level,
+                                    z * level),
                                 f);
                         }
                     }//
@@ -231,7 +256,11 @@ impl<C> Tree<C> where C: VoxelColor {
 //        }
 //    }
 //
-    fn compute_node(&mut self, level: usize, top_left: Pos) -> Node<C> {
+    fn recompute(&mut self) -> Node<C> {
+        self.compute_node(1, Pos { x: 0, y: 0, z: 0 }, Pos { x: 0, y: 0, z: 0 })
+    }
+
+    fn compute_node(&mut self, level: usize, tree_pos: Pos, top_left: Pos) -> Node<C> {
         let lvl_size = level << 1;
         let mut ret = if lvl_size == self.vol.size {
             self.compute_voxel_node(top_left)
@@ -247,11 +276,13 @@ impl<C> Tree<C> where C: VoxelColor {
             for z in 0..2 {
                 for y in 0..2 {
                     for x in 0..2 {
-                        let n = self.compute_node(level << 1, Pos {
-                            x: top_left.x + (x * lvl_size),
-                            y: top_left.y + (y * lvl_size),
-                            z: top_left.z + (z * lvl_size),
-                        });
+                        let n = self.compute_node(
+                            level << 1,
+                            tree_pos.offs(x, y, z),
+                            top_left.offs(
+                                x * lvl_size,
+                                y * lvl_size,
+                                z * lvl_size));
 
                         if !n.empty { all_empty = false; }
                         if let Some(v) = n.voxel {
@@ -289,22 +320,20 @@ impl<C> Tree<C> where C: VoxelColor {
             n
         };
 
-        ret.pos = top_left;
+        ret.pos = tree_pos;
 
-        *self.node(level, top_left) = ret;
+        *self.node(level, tree_pos) = ret;
 
         ret
     }
 
     fn node_at(&self, level: usize, pos: Pos) -> &Node<C> {
-        let offs =
-            if level == 1 { 0 }
-            else { level * level * level };
+        let offs = (level - 1) * (level - 1) * (level - 1);
 
         &self.nodes[
             offs
-            + pos.z * (level >> 1) * (level >> 1)
-            + pos.y * (level >> 1)
+            + pos.z * level * level
+            + pos.y * level
             + pos.x]
     }
 
@@ -313,10 +342,10 @@ impl<C> Tree<C> where C: VoxelColor {
         // 2nd level is: 2x2x2 = 8
         let offs = (level - 1) * (level - 1) * (level - 1);
 
-        println!("ACCESS {},{},{} => {}@{}", pos.x, pos.y, pos.z, 
-            pos.z * level * level
-            + pos.y * level
-            + pos.x, offs);
+//        println!("ACCESS {},{},{} => {}@{}", pos.x, pos.y, pos.z, 
+//            pos.z * level * level
+//            + pos.y * level
+//            + pos.x, offs);
         &mut self.nodes[
             offs
             + pos.z * level * level
@@ -338,11 +367,7 @@ impl<C> Tree<C> where C: VoxelColor {
         for z in 0..2 {
             for y in 0..2 {
                 for x in 0..2 {
-                    let vox = self.vol.get(Pos {
-                        x: top_left.x + x,
-                        y: top_left.y + y,
-                        z: top_left.z + z
-                    });
+                    let vox = self.vol.get(top_left.offs(x, y, z));
 
                     if first { color = vox.color; first = false; }
                     else if color != vox.color { equal_color = false; }
@@ -386,28 +411,28 @@ mod tests {
         let v : Vol<u8> = Vol::new(2);
         let mut t : Tree<u8> = Tree::new(8, v);
         t.fill(0, 0, 0, 2, 2, 2, 10.into());
-        let n = t.compute_node(1, Pos { x: 0, y: 0, z: 0 });
+        let n = t.recompute();
 
         assert_eq!(n.voxel.unwrap().color, 10);
         assert_eq!(n.voxel.unwrap().faces, 0x3F);
         assert_eq!(t.nodes[0].voxel.unwrap().color, 10);
         assert_eq!(t.nodes[0].empty, false);
 
-        let mut first : Option<(usize, usize, usize, usize, Voxel<u8>)> = None;
-        let mut last : Option<(usize, usize, usize, usize, Voxel<u8>)> = None;
-        t.draw(&mut |lvl, x, y, z, v| {
-            if first.is_none() { first = Some((lvl, x, y, z, v)); }
-            last = Some((lvl, x, y, z, v));
+        let mut first : Option<(usize, Pos, Voxel<u8>)> = None;
+        let mut last : Option<(usize, Pos, Voxel<u8>)> = None;
+        t.draw(&mut |lvl, pos, v| {
+            if first.is_none() { first = Some((lvl, *pos, v)); }
+            last = Some((lvl, *pos, v));
         });
         let first = first.unwrap();
-        assert_eq!((first.0, first.1, first.2, first.3), (1, 0, 0, 0));
-        assert_eq!(first.4.color, 10);
-        assert_eq!(first.4.faces, 0x3f); // all faces
+        assert_eq!((first.0, first.1.x, first.1.y, first.1.z), (2, 0, 0, 0));
+        assert_eq!(first.2.color, 10);
+        assert_eq!(first.2.faces, 0x3f); // all faces
 
         let last = last.unwrap();
-        assert_eq!((last.0, last.1, last.2, last.3), (1, 0, 0, 0));
-        assert_eq!(last.4.color, 10);
-        assert_eq!(last.4.faces, 0x3f); // all faces
+        assert_eq!((last.0, last.1.x, last.1.y, last.1.z), (2, 0, 0, 0));
+        assert_eq!(last.2.color, 10);
+        assert_eq!(last.2.faces, 0x3f); // all faces
     }
 
     #[test]
@@ -416,7 +441,7 @@ mod tests {
         let mut t : Tree<u8> = Tree::new(8, v);
         t.fill(0, 0, 0, 2, 2, 2, 10.into());
         t.set(1, 1, 1, 12.into());
-        let n = t.compute_node(1, Pos { x: 0, y: 0, z: 0 });
+        let n = t.recompute();
 
         assert_eq!(n.voxel, None);
         assert_eq!(t.nodes[0].empty, false);
@@ -428,12 +453,78 @@ mod tests {
         let v : Vol<u8> = Vol::new(4);
         let mut t : Tree<u8> = Tree::new(64, v);
         t.fill(0, 0, 0, 4, 4, 4, 10.into());
-        let n = t.compute_node(1, Pos { x: 0, y: 0, z: 0 });
+        let n = t.recompute();
         println!("NODES: {:?}", t.nodes);
 
         assert_eq!(n.voxel.unwrap().color, 10);
         assert_eq!(n.voxel.unwrap().faces, 0x3F);
         assert_eq!(t.nodes[0].voxel.unwrap().color, 10);
         assert_eq!(t.nodes[0].empty, false);
+
+        let mut first : Option<(usize, Pos, Voxel<u8>)> = None;
+        let mut last : Option<(usize, Pos, Voxel<u8>)> = None;
+        t.draw(&mut |size, pos, v| {
+            if first.is_none() { first = Some((size, *pos, v)); }
+            last = Some((size, *pos, v));
+        });
+
+        let first = first.unwrap();
+        assert_eq!((first.0, first.1.x, first.1.y, first.1.z), (4, 0, 0, 0));
+        assert_eq!(first.2.color, 10);
+        assert_eq!(first.2.faces, 0x3f); // all faces
+
+        let last = last.unwrap();
+        assert_eq!((last.0, last.1.x, last.1.y, last.1.z), (4, 0, 0, 0));
+        assert_eq!(last.2.color, 10);
+        assert_eq!(last.2.faces, 0x3f); // all faces
+    }
+
+    #[test]
+    fn check_n2_broken() {
+        let v : Vol<u8> = Vol::new(4);
+        let mut t : Tree<u8> = Tree::new(64, v);
+        t.fill(0, 0, 0, 4, 4, 4, 10.into());
+        t.set(3, 3, 3, 20.into());
+        let n = t.recompute();
+        println!("NODES: {:?}", t.nodes);
+
+        assert_eq!(n.voxel, None);
+        // first layer:
+        assert_eq!(t.nodes[1].voxel.unwrap().color, 10);
+        assert_eq!(t.nodes[1].empty, false);
+        assert_eq!(t.nodes[2].voxel.unwrap().color, 10);
+        assert_eq!(t.nodes[2].empty, false);
+        assert_eq!(t.nodes[3].voxel.unwrap().color, 10);
+        assert_eq!(t.nodes[3].empty, false);
+        assert_eq!(t.nodes[4].voxel.unwrap().color, 10);
+        assert_eq!(t.nodes[4].empty, false);
+
+        // second layer:
+        assert_eq!(t.nodes[5].voxel.unwrap().color, 10);
+        assert_eq!(t.nodes[5].empty, false);
+        assert_eq!(t.nodes[6].voxel.unwrap().color, 10);
+        assert_eq!(t.nodes[6].empty, false);
+        assert_eq!(t.nodes[7].voxel.unwrap().color, 10);
+        assert_eq!(t.nodes[7].empty, false);
+        assert_eq!(t.nodes[8].voxel, None);
+        assert_eq!(t.nodes[8].empty, false);
+
+        let mut first : Option<(usize, Pos, Voxel<u8>)> = None;
+        let mut last : Option<(usize, Pos, Voxel<u8>)> = None;
+        t.draw(&mut |size, pos, v| {
+            println!("DRAWING: size={}, pos={:?} v={:?}", size, pos, v);
+            if first.is_none() { first = Some((size, *pos, v)); }
+            last = Some((size, *pos, v));
+        });
+
+        let first = first.unwrap();
+        assert_eq!((first.0, first.1.x, first.1.y, first.1.z), (2, 0, 0, 0));
+        assert_eq!(first.2.color, 10);
+        assert_eq!(first.2.faces, 0x01 | 0x08 | 0x02); // front, left, top
+
+        let last = last.unwrap();
+        assert_eq!((last.0, last.1.x, last.1.y, last.1.z), (1, 3, 3, 3));
+        assert_eq!(last.2.color, 20);
+        assert_eq!(last.2.faces, 0x04 | 0x10 | 0x20); // back, right, bottom
     }
 }
