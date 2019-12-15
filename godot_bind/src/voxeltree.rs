45181032b011fe78f1f2a719a39e22b88b1dff71
Weird Constructor <weirdconstructor@gmail.com>
2019-12-15 20:35:35 +0100

    octree rendering in debugging

--- godot_bind/src/voxeltree.rs
+++ godot_bind/src/voxeltree.rs
@@ -1,9 +1,19 @@
-trait VoxelColor: PartialEq + Sized + Copy + std::fmt::Debug + Default {}
+pub trait VoxelColor: PartialEq + Sized + Copy + std::fmt::Debug + Default {}
 impl<T: PartialEq + Sized + Copy + std::fmt::Debug + Default> VoxelColor for T {}
 
+type pint = u16;
+
+pub const F_NONE   : u8 = 0x00;
+pub const F_FRONT  : u8 = 0x01;
+pub const F_TOP    : u8 = 0x02;
+pub const F_BACK   : u8 = 0x04;
+pub const F_LEFT   : u8 = 0x08;
+pub const F_RIGHT  : u8 = 0x10;
+pub const F_BOTTOM : u8 = 0x20;
+
 #[derive(Debug, Clone, Copy, PartialEq, Default)]
-struct Voxel<C> where C: VoxelColor {
-    color: C,
+pub struct Voxel<C> where C: VoxelColor {
+    pub color: C,
     /// Bits:
     /// Front,  // x,       y,      z - 1
     /// Top,    // x,       y - 1,  z
@@ -17,20 +27,20 @@ struct Voxel<C> where C: VoxelColor {
     /// - 0x08     - Left
     /// - 0x10     - Right
     /// - 0x20     - Bottom
-    faces: u8,
+    pub faces: u8,
 }
 
 impl<C> Voxel<C> where C: VoxelColor {
 }
 
-fn xyz2facemask(x: usize, y: usize, z: usize) -> u8 {
-    let mut mask : u8 = 0x00;
-    if x == 0 { mask |= 0x08; }
-    else      { mask |= 0x10; }
-    if y == 0 { mask |= 0x02; }
-    else      { mask |= 0x20; }
-    if z == 0 { mask |= 0x01; }
-    else      { mask |= 0x04; }
+fn xyz2facemask(x: pint, y: pint, z: pint) -> u8 {
+    let mut mask : u8 = F_NONE;
+    if x == 0 { mask |= F_LEFT;   }
+    else      { mask |= F_RIGHT;  }
+    if y == 0 { mask |= F_TOP;    }
+    else      { mask |= F_BOTTOM; }
+    if z == 0 { mask |= F_FRONT;  }
+    else      { mask |= F_BACK;   }
     mask
 }
 
@@ -44,9 +54,9 @@ impl Into<Voxel<u8>> for u8 {
 }
 
 #[derive(Debug, Clone, PartialEq, Default)]
-struct Vol<C> where C: VoxelColor {
-    size: usize,
-    data: std::vec::Vec<Voxel<C>>,
+pub struct Vol<C> where C: VoxelColor {
+    pub size: usize,
+    pub data: std::vec::Vec<Voxel<C>>,
 }
 
 impl<C> Vol<C> where C: VoxelColor {
@@ -59,59 +69,62 @@ impl<C> Vol<C> where C: VoxelColor {
         }
     }
 
-    pub fn set(&mut self, x: usize, y: usize, z: usize, v: Voxel<C>) {
-        self.data[z * self.size * self.size + y * self.size + x] = v;
+    pub fn set(&mut self, x: pint, y: pint, z: pint, v: Voxel<C>) {
+        self.data[z as usize * self.size * self.size + y as usize * self.size + x as usize] = v;
     }
 
     pub fn at(&self, pos: Pos) -> &Voxel<C> {
-        &self.data[pos.z * self.size * self.size + pos.y * self.size + pos.x]
+        &self.data[pos.z as usize * self.size * self.size + pos.y as usize * self.size + pos.x as usize]
     }
 
     pub fn get(&mut self, pos: Pos) -> &Voxel<C> {
         let mut faces: u8 = 0x0;
 
         let clr_def = C::default();
-        let size    = self.size;
-        let last    = self.size - 1;
+        let size        = self.size;
+        let last : pint = (self.size - 1) as pint;
 
-        if pos.x == 0    { faces |= 0x08; }
-        if pos.x == last { faces |= 0x10; }
+        if pos.x == 0    { faces |= F_LEFT; }
+        if pos.x == last { faces |= F_RIGHT; }
         if pos.x > 0 && pos.x < last {
-            let clr1 = self.data[pos.z * size * size + pos.y * size + pos.x - 1].color;
-            let clr2 = self.data[pos.z * size * size + pos.y * size + pos.x + 1].color;
+            let clr1 = self.data[pos.z as usize * size * size + pos.y as usize * size + pos.x as usize - 1].color;
+            let clr2 = self.data[pos.z as usize * size * size + pos.y as usize * size + pos.x as usize + 1].color;
             if clr1 == clr_def { faces |= 0x08; }
             if clr2 == clr_def { faces |= 0x10; }
         }
 
-        if pos.y == 0         { faces |= 0x02; }
-        else if pos.y == last { faces |= 0x20; }
+        if pos.y == 0         { faces |= F_TOP; }
+        else if pos.y == last { faces |= F_BOTTOM; }
         if pos.y > 0 && pos.y < last {
-            let clr1 = self.data[pos.z * size * size + (pos.y - 1) * size].color;
-            let clr2 = self.data[pos.z * size * size + (pos.y + 1) * size].color;
-            if clr1 == clr_def { faces |= 0x20; }
-            if clr2 == clr_def { faces |= 0x02; }
+            let clr1 = self.data[pos.z as usize * size * size + (pos.y as usize - 1) * size + pos.x as usize].color;
+            let clr2 = self.data[pos.z as usize * size * size + (pos.y as usize + 1) * size + pos.x as usize].color;
+            if clr1 == clr_def { faces |= F_BOTTOM; }
+            if clr2 == clr_def { faces |= F_TOP; }
         }
 
-        if pos.z == 0         { faces |= 0x01; }
-        else if pos.z == last { faces |= 0x04; }
+        if pos.z == 0         { faces |= F_FRONT; }
+        else if pos.z == last { faces |= F_BACK; }
         if pos.z > 0 && pos.z < last {
-            let clr1 = self.data[(pos.z - 1) * size * size + pos.y * size + pos.x].color;
-            let clr2 = self.data[(pos.z + 1) * size * size + pos.y * size + pos.x].color;
-            if clr1 == clr_def { faces |= 0x01; }
-            if clr2 == clr_def { faces |= 0x04; }
+            let clr1 = self.data[(pos.z as usize - 1) * size * size + pos.y as usize * size + pos.x as usize].color;
+            let clr2 = self.data[(pos.z as usize + 1) * size * size + pos.y as usize * size + pos.x as usize].color;
+            if clr1 == clr_def { faces |= F_FRONT; }
+            if clr2 == clr_def { faces |= F_BACK; }
         }
 
-        let vox = &mut self.data[pos.z * size * size + pos.y * size + pos.x];
+        let vox = &mut self.data[
+                  pos.z as usize * size * size
+                + pos.y as usize * size
+                + pos.x as usize];
         vox.faces = faces;
         vox
     }
 }
 
 #[derive(Clone, Copy, PartialEq, Default)]
-struct Pos {
-    x: usize,
-    y: usize,
-    z: usize,
+pub struct Pos {
+    pub x: pint,
+    pub y: pint,
+    pub z: pint,
 }
 
 impl std::fmt::Debug for Pos {
@@ -122,24 +135,24 @@ impl std::fmt::Debug for Pos {
 }
 
 impl Pos {
-    pub fn new(x: usize, y: usize, z: usize) -> Self {
+    pub fn new(x: pint, y: pint, z: pint) -> Self {
         Self { x, y, z }
     }
 
-    pub fn offs(&self, xo: usize, yo: usize, zo: usize) -> Self {
+    pub fn offs(&self, xo: pint, yo: pint, zo: pint) -> Self {
         Self { x: self.x + xo, y: self.y + yo, z: self.z + zo }
     }
 
-    pub fn mul(&self, m: usize) -> Self {
+    pub fn mul(&self, m: pint) -> Self {
         Self { x: self.x * m, y: self.y * m, z: self.z * m }
     }
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Default)]
-struct Node<C: VoxelColor> {
-    voxel: Option<Voxel<C>>,
-    pos:   Pos,
-    empty: bool,
+pub struct Node<C: VoxelColor> {
+    pub voxel: Option<Voxel<C>>,
+    pub pos:   Pos,
+    pub empty: bool,
 }
 
 impl<C> Node<C> where C: VoxelColor {
@@ -153,19 +166,24 @@ impl<C> Node<C> where C: VoxelColor {
 }
 
 #[derive(Debug, Clone, PartialEq, Default)]
-struct Tree<C: VoxelColor> {
+pub struct Octree<C: VoxelColor> {
     nodes: std::vec::Vec<Node<C>>,
     nodes_size: usize,
-    vol:   Vol<C>,
+    pub vol: Vol<C>,
 }
 
-impl<C> Tree<C> where C: VoxelColor {
-    fn new(node_count: usize, vol: Vol<C>) -> Self {
+impl<C> Octree<C> where C: VoxelColor {
+    pub fn new_from_size(size: usize) -> Self {
+        let v : Vol<C> = Vol::new(size);
+        Octree::new(size * size * size, v)
+    }
+
+    pub fn new(node_count: usize, vol: Vol<C>) -> Self {
         let mut size = vol.size >> 1;
         let mut alloc = 0;
         while size > 0 {
             alloc += size * size * size;
-            println!("SIZ {} {}", size, alloc);
+//            println!("SIZ {} {}", size, alloc);
             size = size >> 1;
         }
         let mut nodes = std::vec::Vec::new();
@@ -189,8 +207,8 @@ impl<C> Tree<C> where C: VoxelColor {
     pub fn draw_level<F>(&self, level: usize, size: usize, tree_pos: Pos, top_left: Pos, f: &mut F)
         where F: FnMut(usize, &Pos, Voxel<C>) -> ()
     {
-        println!("DRAW lvl={}, size={}, tp={:?}, tl={:?}",
-                 level, size, tree_pos, top_left);
+//        println!("DRAW lvl={}, size={}, tp={:?}, tl={:?}",
+//                 level, size, tree_pos, top_left);
         if level == self.vol.size {
 //            for z in 0..2 {
 //                for y in 0..2 {
@@ -215,9 +233,9 @@ impl<C> Tree<C> where C: VoxelColor {
                                 size >> 1,
                                 tree_pos.offs(x, y, z),
                                 top_left.offs(
-                                    x * level,
-                                    y * level,
-                                    z * level),
+                                    x * level as pint,
+                                    y * level as pint,
+                                    z * level as pint),
                                 f);
                         }
                     }//
@@ -226,12 +244,12 @@ impl<C> Tree<C> where C: VoxelColor {
         }
     }
 
-    pub fn set(&mut self, x: usize, y: usize, z: usize, v: Voxel<C>) {
+    pub fn set(&mut self, x: pint, y: pint, z: pint, v: Voxel<C>) {
         self.vol.set(x, y, z, v);
     }
 
-    pub fn fill(&mut self, x: usize, y: usize, z: usize,
-                w: usize, h: usize, d: usize, v: Voxel<C>)
+    pub fn fill(&mut self, x: pint, y: pint, z: pint,
+                w: pint, h: pint, d: pint, v: Voxel<C>)
     {
         for z in z..(z + d) {
             for y in y..(y + h) {
@@ -256,7 +274,7 @@ impl<C> Tree<C> where C: VoxelColor {
 //        }
 //    }
 //
-    fn recompute(&mut self) -> Node<C> {
+    pub fn recompute(&mut self) -> Node<C> {
         self.compute_node(1, Pos { x: 0, y: 0, z: 0 }, Pos { x: 0, y: 0, z: 0 })
     }
 
@@ -280,9 +298,9 @@ impl<C> Tree<C> where C: VoxelColor {
                             level << 1,
                             tree_pos.offs(x, y, z),
                             top_left.offs(
-                                x * lvl_size,
-                                y * lvl_size,
-                                z * lvl_size));
+                                x * lvl_size as pint,
+                                y * lvl_size as pint,
+                                z * lvl_size as pint));
 
                         if !n.empty { all_empty = false; }
                         if let Some(v) = n.voxel {
@@ -290,14 +308,14 @@ impl<C> Tree<C> where C: VoxelColor {
                             else if color != v.color { equal_color = false; }
 
                             faces |= xyz2facemask(x, y, z) & v.faces;
-                            println!("RET: {:?} : {}", n, faces);
+//                            println!("RET: {:?} : {}", n, faces);
                         } else {
                             equal_color = false;
                         }
                     }
                 }
             }
-            dbg!(all_empty, equal_color, color);
+//            dbg!(all_empty, equal_color, color);
 
             let mut n = Node::default();
             if !all_empty && equal_color {
@@ -332,9 +350,9 @@ impl<C> Tree<C> where C: VoxelColor {
 
         &self.nodes[
             offs
-            + pos.z * level * level
-            + pos.y * level
-            + pos.x]
+            + pos.z as usize * level * level
+            + pos.y as usize * level
+            + pos.x as usize]
     }
 
     fn node(&mut self, level: usize, pos: Pos) -> &mut Node<C> {
@@ -348,9 +366,9 @@ impl<C> Tree<C> where C: VoxelColor {
 //            + pos.x, offs);
         &mut self.nodes[
             offs
-            + pos.z * level * level
-            + pos.y * level
-            + pos.x]
+            + pos.z as usize * level * level
+            + pos.y as usize * level
+            + pos.x as usize]
 //        println!("ANOD lvl={}, pos={:?} => {:?}", level, pos, self.nodes);
     }
 
@@ -374,8 +392,8 @@ impl<C> Tree<C> where C: VoxelColor {
                     if color != C::default() { all_empty = false; }
 
                     faces |= xyz2facemask(x, y, z) & vox.faces;
-                    eprintln!("{},{},{} :: {:?} :: {:x} {:x}",
-                              x, y, z, top_left, vox.faces, faces);
+//                    eprintln!("{},{},{} :: {:?} :: {:x} {:x}",
+//                              x, y, z, top_left, vox.faces, faces);
                 }
             }
         }
@@ -409,7 +427,7 @@ mod tests {
     #[test]
     fn check_n1_filled() {
         let v : Vol<u8> = Vol::new(2);
-        let mut t : Tree<u8> = Tree::new(8, v);
+        let mut t : Octree<u8> = Octree::new(8, v);
         t.fill(0, 0, 0, 2, 2, 2, 10.into());
         let n = t.recompute();
 
@@ -438,7 +456,7 @@ mod tests {
     #[test]
     fn check_n1_partial_minus_1() {
         let v : Vol<u8> = Vol::new(2);
-        let mut t : Tree<u8> = Tree::new(8, v);
+        let mut t : Octree<u8> = Octree::new(8, v);
         t.fill(0, 0, 0, 2, 2, 2, 10.into());
         t.set(1, 1, 1, 12.into());
         let n = t.recompute();
@@ -451,10 +469,10 @@ mod tests {
     #[test]
     fn check_n2_filled() {
         let v : Vol<u8> = Vol::new(4);
-        let mut t : Tree<u8> = Tree::new(64, v);
+        let mut t : Octree<u8> = Octree::new(64, v);
         t.fill(0, 0, 0, 4, 4, 4, 10.into());
         let n = t.recompute();
-        println!("NODES: {:?}", t.nodes);
+//        println!("NODES: {:?}", t.nodes);
 
         assert_eq!(n.voxel.unwrap().color, 10);
         assert_eq!(n.voxel.unwrap().faces, 0x3F);
@@ -482,11 +500,11 @@ mod tests {
     #[test]
     fn check_n2_broken() {
         let v : Vol<u8> = Vol::new(4);
-        let mut t : Tree<u8> = Tree::new(64, v);
+        let mut t : Octree<u8> = Octree::new(64, v);
         t.fill(0, 0, 0, 4, 4, 4, 10.into());
         t.set(3, 3, 3, 20.into());
         let n = t.recompute();
-        println!("NODES: {:?}", t.nodes);
+//        println!("NODES: {:?}", t.nodes);
 
         assert_eq!(n.voxel, None);
         // first layer:
@@ -512,7 +530,7 @@ mod tests {
         let mut first : Option<(usize, Pos, Voxel<u8>)> = None;
         let mut last : Option<(usize, Pos, Voxel<u8>)> = None;
         t.draw(&mut |size, pos, v| {
-            println!("DRAWING: size={}, pos={:?} v={:?}", size, pos, v);
+//            println!("DRAWING: size={}, pos={:?} v={:?}", size, pos, v);
             if first.is_none() { first = Some((size, *pos, v)); }
             last = Some((size, *pos, v));
         });
@@ -520,11 +538,75 @@ mod tests {
         let first = first.unwrap();
         assert_eq!((first.0, first.1.x, first.1.y, first.1.z), (2, 0, 0, 0));
         assert_eq!(first.2.color, 10);
-        assert_eq!(first.2.faces, 0x01 | 0x08 | 0x02); // front, left, top
+        assert_eq!(first.2.faces, F_FRONT | F_LEFT | F_TOP);
 
         let last = last.unwrap();
         assert_eq!((last.0, last.1.x, last.1.y, last.1.z), (1, 3, 3, 3));
         assert_eq!(last.2.color, 20);
-        assert_eq!(last.2.faces, 0x04 | 0x10 | 0x20); // back, right, bottom
+        assert_eq!(last.2.faces, F_BACK | F_RIGHT | F_BOTTOM);
+    }
+
+    #[test]
+    fn check_n3_filled() {
+        let v : Vol<u8> = Vol::new(8);
+        let mut t : Octree<u8> = Octree::new(8 * 8 * 8, v);
+        t.fill(0, 0, 0, 8, 8, 8, 11.into());
+        let n = t.recompute();
+
+        assert_eq!(n.voxel.unwrap().color, 11);
+        assert_eq!(n.voxel.unwrap().faces, 0x3F);
+        assert_eq!(t.nodes[0].voxel.unwrap().color, 11);
+        assert_eq!(t.nodes[0].empty, false);
+
+        let mut first : Option<(usize, Pos, Voxel<u8>)> = None;
+        let mut last : Option<(usize, Pos, Voxel<u8>)> = None;
+        t.draw(&mut |size, pos, v| {
+            if first.is_none() { first = Some((size, *pos, v)); }
+            last = Some((size, *pos, v));
+        });
+
+        let first = first.unwrap();
+        assert_eq!((first.0, first.1.x, first.1.y, first.1.z), (8, 0, 0, 0));
+        assert_eq!(first.2.color, 11);
+        assert_eq!(first.2.faces, 0x3f); // all faces
+
+        let last = last.unwrap();
+        assert_eq!((last.0, last.1.x, last.1.y, last.1.z), (8, 0, 0, 0));
+        assert_eq!(last.2.color, 11);
+        assert_eq!(last.2.faces, 0x3f); // all faces
+    }
+
+    #[test]
+    fn check_n256_filled() {
+        let v : Vol<u8> = Vol::new(256);
+        let mut t : Octree<u8> = Octree::new(256 * 256 * 256, v);
+        let d = std::time::Instant::now();
+        t.fill(0, 0, 0, 256, 256, 256, 11.into());
+        eprintln!("TF={}",d.elapsed().as_millis());
+        let n = t.recompute();
+
+        eprintln!("T={}",d.elapsed().as_millis());
+
+        assert_eq!(n.voxel.unwrap().color, 11);
+        assert_eq!(n.voxel.unwrap().faces, 0x3F);
+        assert_eq!(t.nodes[0].voxel.unwrap().color, 11);
+        assert_eq!(t.nodes[0].empty, false);
+
+        let mut first : Option<(usize, Pos, Voxel<u8>)> = None;
+        let mut last : Option<(usize, Pos, Voxel<u8>)> = None;
+        t.draw(&mut |size, pos, v| {
+            if first.is_none() { first = Some((size, *pos, v)); }
+            last = Some((size, *pos, v));
+        });
+
+        let first = first.unwrap();
+        assert_eq!((first.0, first.1.x, first.1.y, first.1.z), (256, 0, 0, 0));
+        assert_eq!(first.2.color, 11);
+        assert_eq!(first.2.faces, 0x3f); // all faces
+
+        let last = last.unwrap();
+        assert_eq!((last.0, last.1.x, last.1.y, last.1.z), (256, 0, 0, 0));
+        assert_eq!(last.2.color, 11);
+        assert_eq!(last.2.faces, 0x3f); // all faces
     }
 }
