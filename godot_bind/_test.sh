2df2aca9bcc24237dbb2acd121576c0e98e2be43
Weird Constructor <weirdconstructor@gmail.com>
2020-01-04 22:41:04 +0100

    got the mining/looking cursor effect and control right

--- godot_bind/_test.sh
+++ godot_bind/_test.sh
@@ -1,6 +1,6 @@
-if cargo build; then
+if cargo build --release; then
     rm ../godot_project/gdnative/libsscg_gd.so
-    cp -v target/debug/libsscg_gd.so ../godot_project/gdnative/
+    cp -v target/release/libsscg_gd.so ../godot_project/gdnative/
     cd ../godot_project/
     ../Godot_v3.1.1-stable_x11.64 project.godot
     cd ../godot_bind/
