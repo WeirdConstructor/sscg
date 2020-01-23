674ec02174332ca0fa73afd0ff01065faed3bb2e
Weird Constructor <weirdconstructor@gmail.com>
2020-01-23 18:58:43 +0100

    reorganized repository a bit

--- _run.sh
+++ _run.sh
@@ -1,9 +1,8 @@
-if cargo build; then
-#;    mv ../godot_project/gdnative/libsscg_gd.so ../godot_project/gdnative/libsscg_gd.so.0
+cd godot_bind
+if cargo build --release; then
     rm ../godot_project/gdnative/libsscg_gd.so
     cp -v target/release/libsscg_gd.so ../godot_project/gdnative/
     cd ../godot_project/
-    #../godot.x11.tools.64
     ../Godot_v3.1.1-stable_x11.64
-    cd ../godot_bind/
 fi
+cd ..
--- _test.sh
+++ _test.sh
@@ -1,7 +1,8 @@
+cd godot_bind
 if cargo build --release; then
     rm ../godot_project/gdnative/libsscg_gd.so
     cp -v target/release/libsscg_gd.so ../godot_project/gdnative/
     cd ../godot_project/
     ../Godot_v3.1.1-stable_x11.64 project.godot
-    cd ../godot_bind/
 fi
+cd ..
