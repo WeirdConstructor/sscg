ccb32ba98d95d0277a9bb90b7729e1d97aee12f0
Weird Constructor <weirdconstructor@gmail.com>
2020-01-31 19:47:13 +0100

    added nemetons music

---
+++ godot_project/scenes/system_map/AudioStreamPlayer.gd
@@ -0,0 +1,30 @@
+extends AudioStreamPlayer
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+const tracks = [
+	"Ryan_Andersen_-_07_-_Synthwave.ogg",
+	"nemeton_preview_v02.ogg",
+];
+
+var i = 0;
+
+func load_next():
+	i = i + 1
+	if i >= len(tracks):
+		i = 0
+	var stream = load('res://music/' + tracks[i])
+	set_stream(stream)
+	play()
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	load_next()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+func _on_AudioStreamPlayer_finished():
+	load_next()
--- update_proj_assets.sh
+++ update_proj_assets.sh
@@ -1,1 +1,2 @@
-cp assets/audio/sounds/*.wav godot_project/sounds/
+cp ../sscg_assets/audio/sounds/*.wav godot_project/sounds/
+cp ../sscg_assets/audio/music/*.ogg godot_project/music/
